481ca7a48d25e237029024e3f8188484ec4947be
volkan.ceylan <volkan.ceylan@marmara.edu.tr>
2015-10-14 22:56:21 +0300

    1.5.7 vsix

--- BuildTemplate.cmd
+++ BuildTemplate.cmd
@@ -1,5 +1,6 @@
 @echo off
 "Serenity\Tools\Cake\Cake.exe" BuildTemplate.cake %*
+"c:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\ide\devenv.exe" Template\Serene.Template.csproj /build
 "c:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\ide\devenv.exe" Template\Serene.Template.csproj /build
 start Template\bin\Debug
 pause
