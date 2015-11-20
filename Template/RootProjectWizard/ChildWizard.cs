3735c00e3bb44b97a0032dd5e816e15a6af550e3
volkan.ceylan <volkanceylan@gmail.com>
2015-11-20 22:56:08 +0200

    auto replace connection string in web.config if LocalDB 2014 installed

--- Template/RootProjectWizard/ChildWizard.cs
+++ Template/RootProjectWizard/ChildWizard.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TemplateWizard;
 using System.Collections.Generic;
+using System.Data.SqlLocalDb;
 
 namespace RootProjectWizard
 {
@@ -9,6 +10,20 @@ namespace RootProjectWizard
         {
             replacementsDictionary["$ext_safeprojectname$"] = RootWizard.GlobalDictionary["$ext_safeprojectname$"];
             replacementsDictionary["$ext_projectname$"] = RootWizard.GlobalDictionary["$ext_projectname$"];
+
+            string localDBInstance = "v11.0";
+            var localDBInstances = SqlLocalDbApi.GetInstanceNames();
+            if (localDBInstances.IndexOf("MSSqlLocalDB") >= 0)
+                localDBInstance = "MSSqlLocalDB";
+            else if (localDBInstances.IndexOf("v12.0") >= 0)
+                localDBInstance = "v12.0";
+            else if (localDBInstances.IndexOf("v11.0") >= 0)
+                localDBInstance = "v11.0";
+            else if (localDBInstances.Count > 0)
+                localDBInstance = localDBInstances[0];
+
+            replacementsDictionary["connectionString=\"Data Source=(LocalDb)\\v11.0;"] =
+                "connectionString=\"Data Source=(LocalDb)\\" + localDBInstance + ";";
         }
 
         public void BeforeOpeningFile(EnvDTE.ProjectItem projectItem)
