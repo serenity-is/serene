fbcec23aba3765f3eba0576d1a080cee4a6ecc9f
volkan.ceylan <volkanceylan@gmail.com>
2015-11-27 22:02:37 +0200

    fix removed users icon (replaced with people)

--- Serene/Serene.Web/Modules/Common/Navigation/NavigationItems.cs
+++ Serene/Serene.Web/Modules/Common/Navigation/NavigationItems.cs
@@ -38,4 +38,4 @@ using Common = Serene.Common.Pages;
 [assembly: NavigationLink(9100, "Administration/Languages", typeof(Administration.LanguageController), icon: "icon-bubbles")]
 [assembly: NavigationLink(9200, "Administration/Translations", typeof(Administration.TranslationController), icon: "icon-speech")]
 [assembly: NavigationLink(9300, "Administration/Roles", typeof(Administration.RoleController), icon: "icon-lock")]
-[assembly: NavigationLink(9400, "Administration/User Management", typeof(Administration.UserController), icon: "icon-users")]
+[assembly: NavigationLink(9400, "Administration/User Management", typeof(Administration.UserController), icon: "icon-people")]
