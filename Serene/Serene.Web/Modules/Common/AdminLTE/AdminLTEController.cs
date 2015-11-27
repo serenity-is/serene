58295302f253a7369d6229c8a5d7b61210ebf0f7
volkan.ceylan <volkanceylan@gmail.com>
2015-11-27 18:52:08 +0200

    restructure adminlte pages

---
+++ Serene/Serene.Web/Modules/Common/AdminLTE/AdminLTEController.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Serene.Common.Pages
+{
+    using System.Web.Mvc;
+
+    [Authorize, RoutePrefix("AdminLTE"), Route("{action=index}")]
+    public class AdminLTEController : Controller
+    {
+        public ActionResult DashboardV2()
+        {
+            return View("~/Modules/Common/AdminLTE/DashboardV2.cshtml");
+        }
+
+        public ActionResult Widgets()
+        {
+            return View("~/Modules/Common/AdminLTE/Widgets.cshtml");
+        }
+    }
+}
---
+++ Serene/Serene.Web/Modules/Common/AdminLTE/Charts/ChartsController.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Serene.Common.Pages
+{
+    using System.Web.Mvc;
+
+    [Authorize, RoutePrefix("AdminLTE/Charts"), Route("{action=index}")]
+    public class ChartsController : Controller
+    {
+        [Route("ChartJS")]
+        public ActionResult ChartJS()
+        {
+            return View("~/Modules/Common/AdminLTE/Charts/ChartJS.cshtml");
+        }
+
+        [Route("Flot")]
+        public ActionResult Flot()
+        {
+            return View("~/Modules/Common/AdminLTE/Charts/Flot.cshtml");
+        }
+
+        [Route("InlineCharts")]
+        public ActionResult InlineCharts()
+        {
+            return View("~/Modules/Common/AdminLTE/Charts/InlineCharts.cshtml");
+        }
+
+        [Route("Morris")]
+        public ActionResult Morris()
+        {
+            return View("~/Modules/Common/AdminLTE/Charts/Morris.cshtml");
+        }
+    }
+}
