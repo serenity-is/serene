8bb55c92238747c37926565f770f18505ff81a42
volkan.ceylan <volkanceylan@gmail.com>
2016-02-04 01:19:58 +0200

    add some notes about permission keys

--- Serene/Serene.Web/Modules/Northwind/PermissionKeys.cs
+++ Serene/Serene.Web/Modules/Northwind/PermissionKeys.cs
@@ -1,6 +1,22 @@
 ﻿
 namespace Serene.Northwind
 {
+    /// <summary>
+    /// This class contains some permission key constants solely for
+    /// easy access and intellisense purposes.
+    /// 
+    /// Please note that you don't have to add permissions here to
+    /// list them in user permissions dialog. Serenity doesn't
+    /// check this class at all.
+    /// 
+    /// To show a new permission in user/role permission dialog, just use
+    /// its string key with ReadPermission / ModifyPermission / 
+    /// DeletePermission / PageAuthorize / ServiceAuthorize etc. attributes 
+    /// and Serenity will auto discover them at application start.
+    /// 
+    /// Permission tree hierarchy in dialog is determined by colons (:)
+    /// in permission keys.
+    /// </summary>
     public class PermissionKeys
     {
         public class Customer
