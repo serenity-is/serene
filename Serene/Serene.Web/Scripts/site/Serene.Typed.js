58a8be6977b3bebd81b57b01d17abfc851e233e0
volkan.ceylan <volkanceylan@gmail.com>
2016-03-20 23:58:22 +0200

    accounttest

--- Serene/Serene.Web/Scripts/site/Serene.Typed.js
+++ Serene/Serene.Web/Scripts/site/Serene.Typed.js
@@ -41,4 +41,3 @@ var Serene;
         Northwind.MyBoldFormatter = MyBoldFormatter;
     })(Northwind = Serene.Northwind || (Serene.Northwind = {}));
 })(Serene || (Serene = {}));
-//# sourceMappingURL=Serene.Typed.js.map
