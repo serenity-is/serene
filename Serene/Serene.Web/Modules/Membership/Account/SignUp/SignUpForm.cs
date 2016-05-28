fcec8a1295640f921f3460fd20e35da485ece46a
chunlizh <chunlizh@live.com>
2016-05-28 15:16:48 +0800

    Update SignUpForm.cs

--- Serene/Serene.Web/Modules/Membership/Account/SignUp/SignUpForm.cs
+++ Serene/Serene.Web/Modules/Membership/Account/SignUp/SignUpForm.cs
@@ -8,15 +8,15 @@ namespace Serene.Membership
     [FormScript("Membership.SignUp")]
     public class SignUpForm
     {
-        [DisplayName("Forms.Membership.SignUp.DisplayName"), Required(true)]
+        [Required(true)]
         public String DisplayName { get; set; }
-        [DisplayName("Forms.Membership.SignUp.Email"), EmailEditor, Required(true)]
+        [EmailEditor, Required(true)]
         public String Email { get; set; }
-        [DisplayName("Forms.Membership.SignUp.ConfirmEmail"), EmailEditor, Required(true)]
+        [EmailEditor, Required(true)]
         public String ConfirmEmail { get; set; }
-        [DisplayName("Forms.Membership.SignUp.Password"), PasswordEditor, Required(true)]
+        [PasswordEditor, Required(true)]
         public String Password { get; set; }
-        [DisplayName("Forms.Membership.SignUp.ConfirmPassword"), PasswordEditor, Required(true)]
+        [PasswordEditor, Required(true)]
         public String ConfirmPassword { get; set; }
     }
 }
