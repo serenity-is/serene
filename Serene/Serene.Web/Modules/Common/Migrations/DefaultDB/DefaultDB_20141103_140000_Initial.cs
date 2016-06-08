8443538d7c349facfc33a42027112e6c6a0e2ec9
volkan.ceylan <volkanceylan@gmail.com>
2016-06-08 23:47:00 +0300

    added pt-BR translations (thanks @rolembergfilho)

--- Serene/Serene.Web/Modules/Common/Migrations/DefaultDB/DefaultDB_20141103_140000_Initial.cs
+++ Serene/Serene.Web/Modules/Common/Migrations/DefaultDB/DefaultDB_20141103_140000_Initial.cs
@@ -2,49 +2,6 @@
 using FluentMigrator.Builders.Create.Table;
 using System;
 
-namespace Serene.Migrations
-{
-    public static class Utils
-    {
-        public static string[] AllExceptOracle =
-        {
-            "SqlServer",
-            "SqlServer2000",
-            "SqlServerCe",
-            "Postgres",
-            "MySql",
-            "Jet",
-            "Sqlite",
-            "SAP HANA"
-        };
-
-        public static void AddOracleIdentity(MigrationBase migration,
-            string table, string id)
-        {
-            var seq = table.Replace(" ", "_").Replace("\"", "");
-            seq = seq.Substring(0, Math.Min(20, seq.Length));
-            seq = seq + "_SEQ";
-
-            migration.IfDatabase("Oracle")
-                .Execute.Sql("CREATE SEQUENCE " + seq);
-
-            migration.IfDatabase("Oracle")
-                .Execute.Sql(String.Format(@"
-CREATE OR REPLACE TRIGGER {2}_TRG
-BEFORE INSERT ON {0}
-FOR EACH ROW
-BEGIN
-	IF :new.{1} IS NULL THEN
-		SELECT {2}.nextval INTO :new.{1} FROM DUAL;
-	END IF;
-END;", table, id, seq));
-
-            migration.IfDatabase("Oracle")
-                .Execute.Sql(@"ALTER TRIGGER " + seq + "_TRG ENABLE");
-        }
-    }
-}
-
 namespace Serene.Migrations.DefaultDB
 {
 
@@ -148,6 +105,12 @@ namespace Serene.Migrations.DefaultDB
             {
                 LanguageId = "pt",
                 LanguageName = "Portuguese"
+            });
+
+            Insert.IntoTable("Languages").Row(new
+            {
+                LanguageId = "pt-BR",
+                LanguageName = "Portuguese (Brazil)"
             });
         }
     }
---
+++ Serene/Serene.Web/Modules/Common/Migrations/MigrationUtils.cs
@@ -0,0 +1,45 @@
+﻿using FluentMigrator;
+using System;
+
+namespace Serene.Migrations
+{
+    public static class Utils
+    {
+        public static string[] AllExceptOracle =
+        {
+            "SqlServer",
+            "SqlServer2000",
+            "SqlServerCe",
+            "Postgres",
+            "MySql",
+            "Jet",
+            "Sqlite",
+            "SAP HANA"
+        };
+
+        public static void AddOracleIdentity(MigrationBase migration,
+            string table, string id)
+        {
+            var seq = table.Replace(" ", "_").Replace("\"", "");
+            seq = seq.Substring(0, Math.Min(20, seq.Length));
+            seq = seq + "_SEQ";
+
+            migration.IfDatabase("Oracle")
+                .Execute.Sql("CREATE SEQUENCE " + seq);
+
+            migration.IfDatabase("Oracle")
+                .Execute.Sql(String.Format(@"
+CREATE OR REPLACE TRIGGER {2}_TRG
+BEFORE INSERT ON {0}
+FOR EACH ROW
+BEGIN
+	IF :new.{1} IS NULL THEN
+		SELECT {2}.nextval INTO :new.{1} FROM DUAL;
+	END IF;
+END;", table, id, seq));
+
+            migration.IfDatabase("Oracle")
+                .Execute.Sql(@"ALTER TRIGGER " + seq + "_TRG ENABLE");
+        }
+    }
+}
