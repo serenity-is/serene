bb412f0371f467b757b0d4759adc455e4a88a536
volkan.ceylan <volkanceylan@gmail.com>
2016-06-08 21:51:28 +0300

    fix error with migration

--- Serene/Serene.Web/Modules/Common/Migrations/DefaultDB/DefaultDB_20160605_215300_Granted.cs
+++ Serene/Serene.Web/Modules/Common/Migrations/DefaultDB/DefaultDB_20160605_215300_Granted.cs
@@ -1,14 +1,15 @@
 ﻿using FluentMigrator;
 
-namespace Serene.Migrations.DefaultDBx
+namespace Serene.Migrations.DefaultDB
 {
     [Migration(20160605215300)]
     public class DefaultDB_20160605_215300_Granted : AutoReversingMigration
     {
         public override void Up()
         {
-            IfDatabase(Utils.AllExceptOracle)
-                .Rename.Column("Grant").OnTable("Users").To("Granted");
+            if (this.Schema.Table("UserPermissions").Column("Grant").Exists())
+                IfDatabase(Utils.AllExceptOracle)
+                    .Rename.Column("Grant").OnTable("UserPermissions").To("Granted");
         }
     }
 }
