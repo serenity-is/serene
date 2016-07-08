678c7cf995ad4bcc5225baaaf32b93416215612d
volkan.ceylan <volkanceylan@gmail.com>
2016-07-08 15:39:27 +0300

    launch gallery edit after build

--- BuildTemplate.cmd
+++ BuildTemplate.cmd
@@ -3,4 +3,5 @@
 "c:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\ide\devenv.exe" Template\Serene.Template.csproj /build
 "c:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\ide\devenv.exe" Template\Serene.Template.csproj /build
 start Template\bin\Debug
+start https://visualstudiogallery.msdn.microsoft.com/559ec6fc-feef-4077-b6d5-5a99408a6681/edit?newSession=True
 pause
