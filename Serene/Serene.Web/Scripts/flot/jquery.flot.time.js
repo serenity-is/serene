4823fb1ef021f4c14fef340c18407522554fc35f
volkan.ceylan <volkan.ceylan@marmara.edu.tr>
2016-07-13 14:58:50 +0300

    converted flot to NuGet reference

--- Serene/Serene.Web/Scripts/flot/jquery.flot.time.js
+++ Serene/Serene.Web/Scripts/flot/jquery.flot.time.js
@@ -1,6 +1,6 @@
 /* Pretty handling of time axes.
 
-Copyright (c) 2007-2013 IOLA and Ole Laursen.
+Copyright (c) 2007-2014 IOLA and Ole Laursen.
 Licensed under the MIT license.
 
 Set axis.mode to "time" to enable. See the section "Time series data" in
@@ -114,8 +114,9 @@ API.txt for details.
 			sourceObj[sourceMethod] = function() {
 				return targetObj[targetMethod].apply(targetObj, arguments);
 			};
-        }
-        var utc = {
+		};
+
+		var utc = {
 			date: d
 		};
 
@@ -136,8 +137,9 @@ API.txt for details.
 		}
 
 		return utc;
-    }
-    // select time zone strategy.  This returns a date-like object tied to the
+	};
+
+	// select time zone strategy.  This returns a date-like object tied to the
 	// desired timezone
 
 	function dateGenerator(ts, opts) {
@@ -425,5 +427,6 @@ API.txt for details.
 	// on the function, so we need to re-expose it here.
 
 	$.plot.formatDate = formatDate;
+	$.plot.dateGenerator = dateGenerator;
 
 })(jQuery);
