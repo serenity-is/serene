d2408d769ee4d57beadb3d0f1cd7bb0b3e267bb5
volkan.ceylan <volkanceylan@gmail.com>
2016-07-17 16:24:42 +0300

    build log

--- BuildTemplate.cmd
+++ BuildTemplate.cmd
@@ -3,7 +3,9 @@
 set VSREGKEY=HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\VisualStudio\14.0
 for /f "skip=2 tokens=2,*" %%A in ('reg query "%VSREGKEY%" /v InstallDir') do SET VSINSTALLDIR=%%B
 Serenity\Tools\NuGet\NuGet Restore Template\Serene.Template.sln
-"%VSINSTALLDIR%devenv.com" Template\Serene.Template.sln /build
+"%VSINSTALLDIR%devenv.exe" "Template\Serene.Template.sln" /build Debug /out "Template\Serene.Template.log"
+type Template\Serene.Template.log
+delete Template\Serene.Template.log
 start Template\bin\Debug
 start https://visualstudiogallery.msdn.microsoft.com/559ec6fc-feef-4077-b6d5-5a99408a6681/edit?newSession=True
 pause
