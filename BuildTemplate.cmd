5b243ba778880e33c9f665de142da0578f25d3ae
volkan.ceylan <volkanceylan@gmail.com>
2016-07-21 19:30:00 +0300

    :up: 2.3.7.1

--- BuildTemplate.cmd
+++ BuildTemplate.cmd
@@ -5,7 +5,7 @@ for /f "skip=2 tokens=2,*" %%A in ('reg query "%VSREGKEY%" /v InstallDir') do SE
 Serenity\Tools\NuGet\NuGet Restore Template\Serene.Template.sln
 "%VSINSTALLDIR%devenv.exe" "Template\Serene.Template.sln" /build Debug /out "Template\Serene.Template.log"
 type Template\Serene.Template.log
-delete Template\Serene.Template.log
+del Template\Serene.Template.log
 start Template\bin\Debug
 start https://visualstudiogallery.msdn.microsoft.com/559ec6fc-feef-4077-b6d5-5a99408a6681/edit?newSession=True
 pause
