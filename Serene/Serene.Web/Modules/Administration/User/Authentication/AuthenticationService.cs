bf24d471e2674070ec2552f28d4ad631f1280732
hannesb <github@hannes-brunner.de>
2016-09-10 14:30:25 +0200

    Update AuthenticationService.cs

--- Serene/Serene.Web/Modules/Administration/User/Authentication/AuthenticationService.cs
+++ Serene/Serene.Web/Modules/Administration/User/Authentication/AuthenticationService.cs
@@ -1,4 +1,4 @@
-﻿namespace Serene.Administration
+namespace Serene.Administration
 {
     using Entities;
     using Repositories;
@@ -105,25 +105,27 @@
             {
                 string salt = user.PasswordSalt.TrimToNull();
                 var hash = UserRepository.GenerateHash(password, ref salt);
-                var displayName = entry.FirstName + " " + entry.LastName;
-                var email = entry.Email.TrimToNull() ?? user.Email ?? (username + "@yourdefaultdomain.com");
+                string displayName, email;
+                ComposeDisplayNameEmail(entry, username, out displayName, out email);
 
                 using (var connection = SqlConnections.NewFor<UserRow>())
-                using (var uow = new UnitOfWork(connection))
                 {
-                    var fld = UserRow.Fields;
-                    new SqlUpdate(fld.TableName)
-                        .Set(fld.DisplayName, displayName)
-                        .Set(fld.PasswordHash, hash)
-                        .Set(fld.PasswordSalt, salt)
-                        .Set(fld.Email, email)
-                        .Set(fld.LastDirectoryUpdate, DateTime.Now)
-                        .WhereEqual(fld.UserId, user.UserId)
-                        .Execute(connection, ExpectedRows.One);
+                    using (var uow = new UnitOfWork(connection))
+                    {
+                        var fld = UserRow.Fields;
+                        new SqlUpdate(fld.TableName)
+                            .Set(fld.DisplayName, displayName)
+                            .Set(fld.PasswordHash, hash)
+                            .Set(fld.PasswordSalt, salt)
+                            .Set(fld.Email, email)
+                            .Set(fld.LastDirectoryUpdate, DateTime.Now)
+                            .WhereEqual(fld.UserId, user.UserId)
+                            .Execute(connection, ExpectedRows.One);
 
-                    uow.Commit();
+                        uow.Commit();
 
-                    UserRetrieveService.RemoveCachedUser(user.UserId, username);
+                        UserRetrieveService.RemoveCachedUser(user.UserId, username);
+                    }
                 }
 
                 return true;
@@ -164,32 +166,61 @@
             {
                 string salt = null;
                 var hash = UserRepository.GenerateHash(password, ref salt);
-                var displayName = entry.FirstName + " " + entry.LastName;
-                var email = entry.Email.TrimToNull() ?? (username + "@yourdefaultdomain.com");
+                string displayName, email;
+                ComposeDisplayNameEmail(entry, username, out displayName, out email);
+#if false
+                // IMHO not a good idea to alter the username (well it's actually the login, not the username)                
                 username = entry.Username.TrimToNull() ?? username;
-
+#endif
+                int? userId;
                 using (var connection = SqlConnections.NewFor<UserRow>())
-                using (var uow = new UnitOfWork(connection))
                 {
-                    var fld = UserRow.Fields;
-                    var userId = (int?)new SqlInsert(fld.TableName)
-                        .Set(fld.Username, username)
-                        .Set(fld.Source, "ldap")
-                        .Set(fld.DisplayName, displayName)
-                        .Set(fld.Email, email)
-                        .Set(fld.PasswordHash, hash)
-                        .Set(fld.PasswordSalt, salt)
-                        .Set(fld.IsActive, 1)
-                        .Set(fld.InsertDate, DateTime.Now)
-                        .Set(fld.InsertUserId, 1)
-                        .Set(fld.LastDirectoryUpdate, DateTime.Now)
-                        .ExecuteAndGetID(connection);
-
-                    uow.Commit();
-
-                    UserRetrieveService.RemoveCachedUser(userId, username);
+                    using (var uow = new UnitOfWork(connection))
+                    {
+                        var fld = UserRow.Fields;
+                        userId = (int?)new SqlInsert(fld.TableName)
+                            .Set(fld.Username, username)
+                            .Set(fld.Source, "ldap")
+                            .Set(fld.DisplayName, displayName)
+                            .Set(fld.Email, email)
+                            .Set(fld.PasswordHash, hash)
+                            .Set(fld.PasswordSalt, salt)
+                            .Set(fld.IsActive, 1)
+                            .Set(fld.InsertDate, DateTime.Now)
+                            .Set(fld.InsertUserId, 1)
+                            .Set(fld.LastDirectoryUpdate, DateTime.Now)
+                            .ExecuteAndGetID(connection);
+                        uow.Commit();
+                        UserRetrieveService.RemoveCachedUser(userId, username);
+                    }
                 }
-
+                if (userId != null)
+                {
+                    // Add user to 'users' role (if role exists)
+                    using (var connection = SqlConnections.NewFor<UserRoleRow>())
+                    {
+                        using (var uow = new UnitOfWork(connection))
+                        {             
+                            // An INSERT .. FROM .. would be better, but I don't know (yet)
+                            // how to code this here               
+                            int? roleId = null;                            
+                            new SqlQuery().From(RoleRow.Fields)
+                                .Select(RoleRow.Fields.RoleId)
+                                .Where(RoleRow.Fields.RoleName == "Users")
+                                .ForFirst(connection, (r) => {
+                                    roleId = r.GetInt32(0);
+                                });
+                            if (roleId != null)
+                            {
+                                new SqlInsert(UserRoleRow.Fields.TableName)
+                                    .Set(UserRoleRow.Fields.UserId, userId)
+                                    .Set(UserRoleRow.Fields.RoleId, roleId)
+                                    .Execute(connection);
+                                uow.Commit();
+                            }
+                        }
+                    }
+                }
                 return true;
             }
             catch (Exception ex)
@@ -197,6 +228,13 @@
                 Log.Error("Error while importing directory user", ex, this.GetType());
                 return false;
             }
+        }
+
+        // This should be done in a unique way
+        private void ComposeDisplayNameEmail(DirectoryEntry entry, string username, out string displayName, out string email)
+        {
+            displayName = (entry.FirstName + " " + entry.LastName).TrimToNull() ?? entry.Username.TrimToNull() ?? username;
+            email = entry.Email.TrimToNull() ?? (((entry.FirstName + " " + entry.LastName).TrimToNull() ?? entry.Username.TrimToNull() ?? username) + "@yourdefaultdomain.com");
         }
     }
 }
