f22d201370d0cde7bfd03498945edb9f4ee57fbe
volkan.ceylan <volkanceylan@gmail.com>
2016-10-24 00:10:38 +0300

    merge ChangePasswordForm into ChangePasswordRequest

--- Serene/Serene.Web/Modules/Membership/Account/ChangePassword/ChangePasswordRequest.cs
+++ Serene/Serene.Web/Modules/Membership/Account/ChangePassword/ChangePasswordRequest.cs
@@ -1,13 +1,18 @@
 ﻿
 namespace Serene.Membership
 {
+    using Serenity.ComponentModel;
     using Serenity.Services;
-    using System;
+    using System.ComponentModel;
 
+    [FormScript("Membership.ChangePassword")]
     public class ChangePasswordRequest : ServiceRequest
     {
-        public String OldPassword { get; set; }
-        public String NewPassword { get; set; }
-        public String ConfirmPassword { get; set; }
+        [PasswordEditor, Required(true), DisplayName("Current Password")]
+        public string OldPassword { get; set; }
+        [PasswordEditor, Required(true), DisplayName("New Password")]
+        public string NewPassword { get; set; }
+        [PasswordEditor, Required(true), DisplayName("Confirm Password")]
+        public string ConfirmPassword { get; set; }
     }
 }
