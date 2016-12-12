d39b4cd9a9e1fc5aad1f9e110909c3f79c7646a6
volkan.ceylan <volkan.ceylan@marmara.edu.tr>
2016-12-12 21:43:01 +0300

    :up: 2.6.10.2

--- Serene/Serene.Web/Scripts/_references.js
+++ Serene/Serene.Web/Scripts/_references.js
@@ -2,7 +2,5 @@
 /// <reference path="jquery-3.1.1.js" />
 /// <autosync enabled="false" />
 /// <reference path="serenity/Serenity.CoreLib.js" />
-/// <reference path="serenity/Serenity.Externals.js" />
-/// <reference path="serenity/Serenity.Externals.Slick.js" />
 /// <reference path="serenity/Serenity.Script.UI.js" />
 /// <reference path="site/Serene.Web.js" />
