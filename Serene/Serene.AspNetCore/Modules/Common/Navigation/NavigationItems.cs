79813f62bc2f475dbec89de6b392f68ec08f97a9
volkan.ceylan <volkan.ceylan@marmara.edu.tr>
2017-01-16 09:40:39 +0300

    removing exceptions log link as StackExchange.Exceptional doesn't support .NET Core yet

--- Serene/Serene.AspNetCore/Modules/Common/Navigation/NavigationItems.cs
+++ Serene/Serene.AspNetCore/Modules/Common/Navigation/NavigationItems.cs
@@ -4,7 +4,6 @@ using Administration = Serene.Administration.Pages;
 [assembly: NavigationLink(1000, "Dashboard", url: "~/", permission: "", icon: "icon-speedometer")]
 
 [assembly: NavigationMenu(9000, "Administration", icon: "icon-screen-desktop")]
-[assembly: NavigationLink(9000, "Administration/Exceptions Log", url: "~/errorlog.axd", permission: Serene.Administration.PermissionKeys.Security, icon: "icon-ban", Target = "_blank")]
 [assembly: NavigationLink(9000, "Administration/Languages", typeof(Administration.LanguageController), icon: "icon-bubbles")]
 [assembly: NavigationLink(9000, "Administration/Translations", typeof(Administration.TranslationController), icon: "icon-speech")]
 [assembly: NavigationLink(9000, "Administration/Roles", typeof(Administration.RoleController), icon: "icon-lock")]
