1193a20ca74f674f2bb3fef916f4e7199c41a102
volkan.ceylan <volkanceylan@gmail.com>
2017-02-15 20:01:44 +0300

    :up: 2.9.5.2

--- Serene/Serene.Web/Modules/Administration/AdministrationNavigation.cs
+++ Serene/Serene.Web/Modules/Administration/AdministrationNavigation.cs
@@ -5,6 +5,5 @@ using Administration = Serene.Administration.Pages;
 [assembly: NavigationLink(9000, "Administration/Exceptions Log", url: "~/errorlog.axd", permission: Serene.Administration.PermissionKeys.Security, icon: "icon-ban", Target = "_blank")]
 [assembly: NavigationLink(9000, "Administration/Languages", typeof(Administration.LanguageController), icon: "icon-bubbles")]
 [assembly: NavigationLink(9000, "Administration/Translations", typeof(Administration.TranslationController), icon: "icon-speech")]
-[assembly: NavigationLink(9000, "Administration/Sergen", typeof(Administration.SergenController), icon: "icon-magic-wand")]
 [assembly: NavigationLink(9000, "Administration/Roles", typeof(Administration.RoleController), icon: "icon-lock")]
 [assembly: NavigationLink(9000, "Administration/User Management", typeof(Administration.UserController), icon: "icon-people")]
