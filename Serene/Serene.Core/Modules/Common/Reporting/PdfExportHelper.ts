8115fa80f9f7f56ca01355912b130bf86dc27860
volkanceylan <volkanceylan@gmail.com>
2017-07-30 20:15:22 +0300

    backport some changes

--- Serene/Serene.Core/Modules/Common/Reporting/PdfExportHelper.ts
+++ Serene/Serene.Core/Modules/Common/Reporting/PdfExportHelper.ts
@@ -159,7 +159,7 @@ namespace Serene.Common {
                         };
                         autoOptions.afterPageContent = footer;
                     }
-
+                    
                     // Print header of page
                     if (options.printDateTimeHeader == null || options.printDateTimeHeader) {
                         var beforePage = function (data) {
@@ -167,8 +167,8 @@ namespace Serene.Common {
                             doc.setFontSize(8);
 
                             // Date and time of the report
-                            doc.autoTableText(Q.formatDate(new Date(), "dd-MM-yyyy HH:mm"),
-                                doc.internal.pageSize.width - autoOptions.margin.right, 13,
+                            doc.autoTableText(Q.formatDate(new Date(), "dd-MM-yyyy HH:mm"), 
+                                doc.internal.pageSize.width - autoOptions.margin.right, 13, 
                                 {
                                     halign: 'right'
                                 });
