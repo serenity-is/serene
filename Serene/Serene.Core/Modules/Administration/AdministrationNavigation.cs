489b4f19262d2021903ee66b56ee9bd44d73f313
volkan.ceylan <volkan.ceylan@marmara.edu.tr>
2017-10-21 21:47:10 +0300

    use font awesome instead of simple line icons in navigation to remove dependency on simple line icons

--- Serene/Serene.Core/Modules/Administration/AdministrationNavigation.cs
+++ Serene/Serene.Core/Modules/Administration/AdministrationNavigation.cs
@@ -1,9 +1,9 @@
 ﻿using Serenity.Navigation;
 using Administration = Serene.Administration.Pages;
 
-[assembly: NavigationMenu(9000, "Administration", icon: "icon-screen-desktop")]
-[assembly: NavigationLink(9000, "Administration/Languages", typeof(Administration.LanguageController), icon: "icon-bubbles")]
-[assembly: NavigationLink(9000, "Administration/Translations", typeof(Administration.TranslationController), icon: "icon-speech")]
-[assembly: NavigationLink(9000, "Administration/Sergen", typeof(Administration.SergenController), icon: "icon-magic-wand")]
-[assembly: NavigationLink(9000, "Administration/Roles", typeof(Administration.RoleController), icon: "icon-lock")]
-[assembly: NavigationLink(9000, "Administration/User Management", typeof(Administration.UserController), icon: "icon-people")]
+[assembly: NavigationMenu(9000, "Administration", icon: "fa-desktop")]
+[assembly: NavigationLink(9000, "Administration/Languages", typeof(Administration.LanguageController), icon: "fa-comments")]
+[assembly: NavigationLink(9000, "Administration/Translations", typeof(Administration.TranslationController), icon: "fa-comment-o")]
+[assembly: NavigationLink(9000, "Administration/Sergen", typeof(Administration.SergenController), icon: "fa-magic")]
+[assembly: NavigationLink(9000, "Administration/Roles", typeof(Administration.RoleController), icon: "fa-lock")]
+[assembly: NavigationLink(9000, "Administration/User Management", typeof(Administration.UserController), icon: "fa-users")]
