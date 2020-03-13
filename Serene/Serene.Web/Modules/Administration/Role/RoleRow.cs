4b03007dadacfe912c1161a2896ea515bb0cd720
hannesb <github@hannes-brunner.de>
2020-03-13 17:36:05 +0100

    Delete unnecessary attributes for RoleId (#68)
    Probably a copy&paste error?

--- Serene/Serene.Web/Modules/Administration/Role/RoleRow.cs
+++ Serene/Serene.Web/Modules/Administration/Role/RoleRow.cs
@@ -14,7 +14,7 @@ namespace Serene.Administration.Entities
     [LookupScript]
     public sealed class RoleRow : Row, IIdRow, INameRow
     {
-        [DisplayName("Role Id"), Identity, ForeignKey("Roles", "RoleId"), LeftJoin("jRole")]
+        [DisplayName("Role Id"), Identity]
         public Int32? RoleId
         {
             get { return Fields.RoleId[this]; }
