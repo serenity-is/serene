5ce64e2b9a5f30f7dbde99c60fe0de572af99b81
volkanceylan <volkanceylan@gmail.com>
2023-11-01 00:33:59 +0300

    backport login page as a jsx-dom sample, add preact/jsx-dom to package.json

---
+++ src/Serene.Web/Modules/Membership/Account/AccountPanelTitle.tsx
@@ -0,0 +1,7 @@
+/** @jsxImportSource jsx-dom */
+import { Texts } from "@/ServerTypes/Texts";
+
+export const AccountPanelTitle = () =>
+    <h2 class="text-center p-4">
+        <img class="s-site-logo-img s-form-title-logo" /> {Texts.Forms.SiteTitle}
+    </h2>
